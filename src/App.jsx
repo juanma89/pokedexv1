100f550b5f007f0b77e4b0d848b2d33a207e0108
juanper <juan.peresistemz@gmail.com>
2025-03-04 20:07:09 -0600

    Custom Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from "react"
-// import Texto from "./components/Texto"
-// import Contador from "./components/Contador"
-// import ListaNombres from "./components/ListaNombre"
+//import Texto from "./components/Texto"
+//import Contador from "./components/Contador"
+//import ListaNombres from "./components/ListaNombre"
 import Pokemones from "./components/pokemones"
+//import Referencia from "./components/referencia"
 
-
-function App (){
-    return <div>
-        <Pokemones/>
-    </div>
-}
-
-// const App1 = () => {
+// function App (){
 //     return <div>
-//         {/* <Texto name="Juan" apellido="Pérez" edad={35}/>
-//         <Texto name="Raúl" apellido="Romero" edad={45}/>
-//         <Contador/>*/}
-//         {/* <Contador/> */}
-//         {/* <ListaNombres/> */}
-//         {/* <Pokemones/> */}
+//         <Pokemones/>
 //     </div>
 // }
 
-export default App;
+const App1 = () => {
+    return <div>
+        {/* <Texto name="Juan" apellido="Pérez" edad={35}/>
+        <br/>
+        <Texto name="Raúl" apellido="Romero" edad={45}/>
+        <br/>
+        <Contador/>
+        <br/>
+        <Contador/>
+        <br/>
+        <ListaNombres/>
+        <br/> */}
+        <Pokemones/>
+        {/* <br/>
+        <Referencia/> */}
+    </div>
+}
+
+export default App1;
---
+++ src/components/referencia.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+
+const Referencia = () => {
+  const inputRef = useRef();
+
+  const handleFocus = () => {
+    const input = document.getElementById("input-focus");
+    input.focus();
+  };
+
+  console.log(inputRef);
+
+  return (
+    <div>
+      <h1>Referencia</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={() => inputRef.current.focus()}>Focus</button>
+      <br/>
+      <input type="text" id='input-focus' />
+      <button onClick={handleFocus}>Focus 2</button>
+    </div>
+  );
+};
+
+export default Referencia;
